0e651c64e8c6ae54859702138d70fe462be184a1
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-06-10 10:44:29 -0300

    Estilizando componente

--- src/components/MenuSideBar/styles.ts
+++ src/components/MenuSideBar/styles.ts
@@ -2,13 +2,62 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    width: "60%",
-    backgroundColor: "#9A5CD1",
-    elevation: 3,
+    width: "100%",
+    height: '8.8%',
+    backgroundColor: "#0400BE",
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 8
-  }
+    borderTopRightRadius: 15,
+    borderTopLeftRadius: 15,
+    position: 'absolute', 
+    left: 0, 
+    right: 0, 
+    bottom: 0
+  },
+
+  containerOptions: {
+    width: '90%',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+
+  icons: {
+    width: 30,
+    height: 30
+  },
+
+  optionsActive: {
+    color: '#FDE46E',
+    fontSize: 10
+  },
+
+  optionsDeactive: {
+    color: '#fff',
+    fontSize: 10
+  },
+
+  optionsDeactiveAdd: {
+    marginBottom: 5,
+    color: '#0400BE',
+    fontSize: 40
+  },
+
+  boxOptions: {
+    marginTop: 14,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  boxOptionsAdd: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 64,
+    height: 64,
+    backgroundColor: '#FDE46E',
+    borderRadius: 50
+  },
+
+
 });
 
 export { styles };
