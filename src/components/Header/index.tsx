02dc39df5cca40d8b6b8892272e1ee6554a45f44
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-06-21 09:08:59 -0300

    Criando interface props

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,28 @@
 import React, { useState } from 'react'
-import { View, Text, TouchableOpacity, Image, SafeAreaView } from 'react-native'
+import { View, Text, TouchableOpacity, Image, SafeAreaView, StatusBar } from 'react-native'
 import { styles } from './styles';
 
 import filterIcon from '../../../assets/icons/filter-icon.png';
 import favoriteIcon from '../../../assets/icons/favorite-icon.png';
 import cartIcon from '../../../assets/icons/cart-icon.png';
 
-const Header = () => {
+interface IHeaderProps {
+    title1: string;
+    title2: string;
+}
+
+const Header = ({title1, title2} : IHeaderProps) => {
 
     return (
         <SafeAreaView style={styles.container}>
+            <StatusBar  barStyle="light-content" backgroundColor="#0400BE" translucent />
 
             <View style={styles.containerHeader}>
 
                 <View style={styles.titlePage}>
-                    <Text style={styles.title}>Home</Text>
+                    <Text style={styles.title}>{title1}</Text>
                     <Text style={styles.divisor}>•</Text>
-                    <Text style={styles.subtitle}>Feed</Text>
+                    <Text style={styles.subtitle}>{title2}</Text>
                 </View>
 
                 <View style={styles.containerOptionsHeader}>
