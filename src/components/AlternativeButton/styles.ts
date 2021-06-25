10427664e24c7af1509fdb44e4c5ebc372342b6b
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-06-24 21:43:24 -0300

    Criando novo botao alternativo

---
+++ src/components/AlternativeButton/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  button: {
+    width: "60%",
+    backgroundColor: "#9347F6",
+    elevation: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 20
+
+  },
+  textButton: {
+    fontFamily: 'Roboto_700Bold',
+    fontSize: 20,
+    color: "#fff",
+    marginVertical: 8
+  }
+});
+
+export { styles };
