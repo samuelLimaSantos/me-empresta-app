dbc82e07ff6402ab51d0cd200fcf82bf6f04c294
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-06-24 23:43:55 -0300

    Criando pagina de carrinho

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,26 +1,129 @@
 import React, { useState } from "react";
-import { View, Text, KeyboardAvoidingView, ImageBackground, TouchableOpacity, SafeAreaView } from "react-native";
+import { View, Text, KeyboardAvoidingView, ImageBackground, TouchableOpacity, Image, SafeAreaView } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
 import { styles } from "./styles";
 import { Header } from "../../components/Header";
 import { MenuSideBar } from "../../components/MenuSideBar";
-import { SimpleInput } from "../../components/SimpleInput";
+
 import astronautaLogin from "../../../assets/astronauta-login.png";
+
+import { Ionicons } from '@expo/vector-icons';
+import CheckBox from '@react-native-community/checkbox';
+
 
 const Cart = () => {
 
   const navigation = useNavigation();
-  const [search, setSearch] = useState(false);
+  const [toggleCheckBox, setToggleCheckBox] = useState(false);
+  const [count, setCount] = useState(0);
 
   return (
     <SafeAreaView style={styles.container}>
       <Header title1="Carrinho" title2="Compras" />
-      <SafeAreaView>
-        <ScrollView showsVerticalScrollIndicator={false} style={styles.containerProdutos}>
+      <SafeAreaView style={styles.containerItens}>
+        <Text style={styles.label}>Total de itens no carrinho: 3</Text>
+        <ScrollView showsVerticalScrollIndicator={false} >
+          <SafeAreaView style={styles.containerList}>
+            <SafeAreaView style={styles.containerDetailProductCart}>
+              <View>
+                <Image source={astronautaLogin} style={styles.productImage} />
+              </View>
+
+              <View style={styles.boxNameDelivery}>
+                <Text style={styles.textProduct}>Samsung S10+</Text>
+                <Text style={styles.textDelivery}>Entrega presencial</Text>
+              </View>
+
+              <Text style={styles.textDays}>20 dias</Text>
+
+              <Text style={styles.textPrice}>{`R$ 20,00`}</Text>
+
+              <CheckBox
+                disabled={false}
+                value={toggleCheckBox}
+                onValueChange={(newValue) => setToggleCheckBox(newValue)}
+              />
+
+              <Ionicons name="trash-outline" size={24} color="red" style={styles.iconTrash} />
+            </SafeAreaView>
+
+            <SafeAreaView style={styles.containerDetailProductCart}>
+              <View>
+                <Image source={astronautaLogin} style={styles.productImage} />
+              </View>
+
+              <View style={styles.boxNameDelivery}>
+                <Text style={styles.textProduct}>Samsung S10+</Text>
+                <Text style={styles.textDelivery}>Entrega presencial</Text>
+              </View>
+
+              <Text style={styles.textDays}>20 dias</Text>
+
+              <Text style={styles.textPrice}>{`R$ 20,00`}</Text>
+
+              <CheckBox
+                disabled={false}
+                value={toggleCheckBox}
+                onValueChange={(newValue) => setToggleCheckBox(newValue)}
+              />
+
+              <Ionicons name="trash-outline" size={24} color="red" style={styles.iconTrash} />
+            </SafeAreaView>
+
+            <SafeAreaView style={styles.containerDetailProductCart}>
+              <View>
+                <Image source={astronautaLogin} style={styles.productImage} />
+              </View>
+
+              <View style={styles.boxNameDelivery}>
+                <Text style={styles.textProduct}>Samsung S10+</Text>
+                <Text style={styles.textDelivery}>Entrega presencial</Text>
+              </View>
+
+              <Text style={styles.textDays}>20 dias</Text>
+
+              <Text style={styles.textPrice}>{`R$ 20,00`}</Text>
+
+              <CheckBox
+                disabled={false}
+                value={toggleCheckBox}
+                onValueChange={(newValue) => setToggleCheckBox(newValue)}
+              />
+
+              <Ionicons name="trash-outline" size={24} color="red" style={styles.iconTrash} />
+            </SafeAreaView>
 
 
+
+          </SafeAreaView>
         </ScrollView>
+        <SafeAreaView style={styles.boxTotal}>
+          <Text style={styles.textTotal}>Total</Text>
+          <Text style={styles.textPriceTotal}>{`R$ 1650,00`}</Text>
+
+          <SafeAreaView style={styles.containerOptions} >
+            <View style={styles.boxCheckbox}>
+              <CheckBox
+                disabled={false}
+                value={toggleCheckBox}
+                onValueChange={(newValue) => setToggleCheckBox(newValue)}
+              />
+              <Text>Tudo</Text>
+            </View>
+
+            <TouchableOpacity style={styles.fakeButton}>
+              <Text style={styles.textButton}>Continuar comprando</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.fakeButtonAlternative}>
+              <Text style={styles.textButtonAlternative}>Finalizar compra</Text>
+            </TouchableOpacity>
+
+          </SafeAreaView>
+        </SafeAreaView>
+
+
       </SafeAreaView>
 
       <MenuSideBar />
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -6,14 +6,148 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight,
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
   },
 
-  containerProdutos: {
+  containerItens: {
     width: 385,
     height: 670,
     marginTop: 85,
     marginBottom: 50
+  },
+
+  label: {
+    color: '#5C2A9D',
+    fontWeight: 'bold',
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 10
+  },
+
+  containerList: {
+    alignItems: 'center',
+    marginTop: 10
+  },
+
+  productImage: {
+    width: 50,
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#5C2A9D',
+    borderRadius: 4,
+  },
+
+
+  containerDetailProductCart: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+    justifyContent: 'space-between'
+  },
+
+  boxNameDelivery: {
+    width: 114,
+    marginLeft: 10,
+  },
+
+  textProduct: {
+    color: '#5C2A9D',
+    fontSize: 14,
+    marginLeft: 5,
+    fontWeight: 'bold'
+  },
+
+  textDelivery: {
+    color: '#455A64',
+    fontSize: 12,
+    marginLeft: 5,
+    fontWeight: 'bold'
+  },
+
+  textDays: {
+    color: '#455A64',
+    fontSize: 12,
+    marginLeft: 5,
+    marginRight: 10,
+    fontWeight: 'bold'
+  },
+
+  textPrice: {
+    color: '#5C2A9D',
+    fontSize: 14,
+    marginLeft: 10,
+    marginRight: 10,
+    fontWeight: 'bold'
+  },
+
+  iconTrash: {
+    marginLeft: 10,
+    marginRight: 10
+  },
+
+  boxTotal: {
+    alignItems: 'center',
+    marginBottom: 80
+  },
+
+  textTotal: {
+    color: '#455A64',
+    fontSize: 32,
+    fontWeight: 'bold'
+  },
+
+  textPriceTotal: {
+    color: '#5C2A9D',
+    fontSize: 32,
+    marginTop: 5,
+    marginBottom: 5,
+    fontWeight: 'bold'
+  },
+
+  containerOptions: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginBottom: 20,
+    marginTop: 40,
+    width: '100%'
+  },
+
+  boxCheckbox: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginLeft: 20,
+    marginRight: 58
+  },
+
+  fakeButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#5C2A9D',
+    borderRadius: 4,
+    padding: 10,
+    marginLeft: 10,
+    marginRight: 10
+  },
+
+  textButton: {
+    color: '#fff',
+    fontSize: 10,
+  },
+
+  fakeButtonAlternative: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FDE46E',
+    borderRadius: 4,
+    padding: 10,
+    marginLeft: 10,
+    marginRight: 10
+  },
+
+  textButtonAlternative: {
+    color: '#5C2A9D',
+    fontSize: 10,
+    fontWeight: 'bold'
   },
 
 });
